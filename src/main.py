4f6cac9013e3e643f9424d99ce90f453bdf7a279
midori <pigsdofly6@gmail.com>
2019-03-26 13:22:48 +0000

    Created server.py file

--- src/main.py
+++ src/main.py
@@ -3,5 +3,11 @@ import delegate
 from bluepy import btle, thingy52
 
 if __name__ == '__main__':
-    thingy = thingy52.Thingy52(config.MAC_ADDRESS)
-
+    try:
+        thingy = thingy52.Thingy52(config.MAC_ADDRESS)
+        thingy.setDelegate(delegate.DelegateHandler())
+        while(True):
+            thingy.waitForNotifications(timeout=10)
+    finally:
+        thingy.disconnect()
+        del thingy
---
+++ src/server.py
@@ -0,0 +1,15 @@
+import socket
+from config import HOST, PORT
+
+def open_server():
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.bind((HOST, PORT))
+        s.listen()
+        conn, addr = s.accept()
+        with conn:
+            print('Connected by', addr)
+            while True:
+                data = conn.recv(1024)
+                if not data:
+                    break
+                conn.sendall(data)
